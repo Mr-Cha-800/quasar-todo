7c32673d20ec4097fe17d931c1366636975e69ec
Mr-Cha-800 <chakerdmlc@gmail.com>
2020-11-03 22:08:44 +0100

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg" >
+ <h5 class="q-mt-none">Help</h5>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sunt voluptates temporibus accusamus beatae, impedit amet, quia nemo voluptatum, facilis aut veritatis! Eos necessitatibus soluta maxime sint aspernatur, fugit dicta.</p>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sunt voluptates temporibus accusamus beatae, impedit amet, quia nemo voluptatum, facilis aut veritatis! Eos necessitatibus soluta maxime sint aspernatur, fugit dicta.</p>
+   </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
